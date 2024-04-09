9cec1d09b369b6935e5fdf001eb6d0a2cee968eb
moreira8125 <your_github_email>
2024-04-09 20:01:57 +0100

    structure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-import Homepage from './pages/Homepage'
-import AllEvents from './pages/AllEvents'
-import OneEvent from './pages/OneEvent'
-import AddEvent from './pages/AddEvent'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router-dom'
+import "./App.css";
+import Homepage from "./pages/Homepage";
+import AllEvents from "./pages/AllEvents";
+import OneEvent from "./pages/OneEvent";
+import AddEvent from "./pages/AddEvent";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Navbar from "./components/Navbar";
+import UserProfile from "./pages/UserProfile";
+
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-    <Navbar/>
-    <Routes>
-      <Route element={<Homepage/>} path='/'></Route>
-      <Route element={<AllEvents/>} path='/events'></Route>
-      <Route element={<OneEvent/>} path='/events/:id'></Route>
-      <Route element={<AddEvent/>} path='/new-event'></Route>
-      <Route element={<Login/>} path='/login'></Route>
-      <Route element={<Signup/>} path='/signup'></Route>
-    </Routes>
-    
+      <Navbar />
+      <Routes>
+        <Route element={<Homepage />} path="/"></Route>
+        <Route element={<AllEvents />} path="/events"></Route>
+        <Route element={<OneEvent />} path="/events/:id"></Route>
+        <Route element={<AddEvent />} path="/new-event"></Route>
+        <Route element={<Login />} path="/login"></Route>
+        <Route element={<Signup />} path="/signup"></Route>
+        <Route element={<UserProfile />} path="/profile/:id"></Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
