3ea22fe3bb686bc9391312003df8e190b46918a1
moreira8125 <your_github_email>
2024-04-09 20:23:21 +0100

    added links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,27 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { useState } from "react";
-import logo from "../assets/Logo1Transparent2.png"
+import logo from "../assets/Logo1Transparent2.png";
 
 function Navbar() {
-
   return (
     <div className="bg-green3_color flex w-full sticky top-0 justify-between flex-wrap pt-4">
       <img src={logo} className="w-48 ml-8 md:w-64 lg:w-96" />
-<div className="xl:flex xl:pt-4 xl:mr-24 xl:gap-28 xl:mt-4">
-<NavLink>
-  Home
-</NavLink>
-<NavLink>
-  Events
-</NavLink>
-<NavLink>
-  Add an event
-</NavLink>
+      <div className="xl:flex xl:pt-4 xl:mr-24 xl:gap-28 xl:mt-4">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/events">Events</NavLink>
+        <NavLink to="/new-event">Add an event</NavLink>
 
-<NavLink>
-  User Profile
-</NavLink>
+        <NavLink to="/profile/:id">User Profile</NavLink>
 
-<NavLink>
-  Log In
-</NavLink>
+        <NavLink to="/login">Log In</NavLink>
 
-<NavLink>
-Sign Up
-</NavLink>
-
-
-</div>
+        <NavLink to="/signup">Sign Up</NavLink>
+      </div>
 
       <div></div>
-
-
     </div>
-    
-  )
+  );
 }
 
 export default Navbar;
--- src/pages/AddEvent.jsx
+++ src/pages/AddEvent.jsx
@@ -1,47 +1,54 @@
-import axios from 'axios'
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function AddEvent() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const [name,setName] = useState('')
-  const [date,setDate] = useState('')
-  const [description,setDescription] = useState('')
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [description, setDescription] = useState("");
 
-  const newEvent ={name,date,description}
+  const newEvent = { name, date, description };
 
-  function handleSubmit(e){
-    e.preventDefault()
-    axios.post(`http://localhost:5005/api/events`, newEvent)
-    .then((event)=>{
-      navigate(`/events/${event.data._id}`)
-      console.log(event)
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios
+      .post(`http://localhost:5005/api/events`, newEvent)
+      .then((event) => {
+        navigate(`/events/${event.data._id}`);
+        console.log(event);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label>Name</label>
-        <input type="text" onChange={(e)=>setName(e.target.value)} />
+        <input type="text" onChange={(e) => setName(e.target.value)} />
       </div>
       <div>
         <label>Date</label>
-        <input type="date" onChange={(e)=>{setDate(e.target.value) ;console.log(e.target.value)}} />
+        <input
+          type="date"
+          onChange={(e) => {
+            setDate(e.target.value);
+            console.log(e.target.value);
+          }}
+        />
       </div>
       <div>
         <label>Description</label>
-        <input type="text" onChange={(e)=>setDescription(e.target.value)} />
+        <input type="text" onChange={(e) => setDescription(e.target.value)} />
       </div>
       <div>
         <button>Submit event</button>
       </div>
     </form>
-  )
+  );
 }
 
-export default AddEvent
+export default AddEvent;
