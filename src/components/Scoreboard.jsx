cab6e06e051b90307ef7ab684751aafa56b00689
Eleanor Pitcher <eleanorpitcher@gmail.com>
2024-04-27 13:01:32 +0200

    scoreboard styling

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -24,31 +24,51 @@ function Scoreboard() {
     },[])
 
     return (
-        <div className='flex justify-center mt-8 text-center'>
-            <div>
-                <h1>Scoreboard</h1>
+
+            <div className='flex flex-col items-center min-h-screen' style={{backgroundColor: '#F5FBEF'}}>
+            <div className='text-center'>
+                <h1 className='text-6xl pt-10 pb-5 font-bold'>Scoreboard</h1>
             </div>
-            <div className="w-full lg:w-3/4">
-                <table className='w-full border-collapse border border-gray-400'>
-                <thead>
-                <tr className="border border-gray-400 px-4 py-2">
-                    <th>#</th>
-                    <th>Username</th>
-                    <th>Score</th>
-                </tr>
-                </thead>
-                <tbody>
-                {players.map((player, index) => (
-                    <tr key={player.id} className="border border-gray-400 px-4 py-2">
-                        <td>{index + 1}</td>
-                        <td>{player.username}</td>
-                        <td>{player.totalScore}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg lg:w-3/4">
+                {/* <div className="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white">
+                    <label htmlFor="table-search" className="sr-only">Search</label>
+                </div> */}
+                <table className="w-full text-sm text-left rtl:text-right ">
+                    <thead className="text-xs text-gray-700 uppercase" style={{backgroundColor: '#A4B7A4'}}>
+                        <tr>
+                            <th scope="col" className="px-6 py-3">
+                                #
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Player
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Score
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {players.map((player, index) => (
+                            <tr className={`border-b border-gray-700 ${index === 0 ? 'bg-green-600' : ''}`} key={player.id}>
+                                <td className="px-6 py-4 items-center">
+                                    {index + 1}
+                                </td>
+                                <td scope="row" className="flex items-center px-6 py-4 whitespace-nowrap dark:text-white">
+                                    <img className="w-10 h-10 rounded-full" src={player.profilePhoto} alt={`${player.name} image`} />
+                                    <div className="ps-3">
+                                        <div className="text-base font-semibold">{player.name}</div>
+                                        <div className="font-normal text-gray-500">@{player.username}</div>
+                                    </div>  
+                                </td>
+                                <td className="px-6 py-4 items-center">
+                                    {player.totalScore}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
-        </div>
+            </div>
       );
       
 }
